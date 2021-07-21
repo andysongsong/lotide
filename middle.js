10a390403a700ee6848bc4aa18fea2901dc9a3d0
andysongsong <sxj930916@gmail.com>
2021-07-21 21:57:11 +0000

    middle

---
+++ middle.js
@@ -0,0 +1,40 @@
+//
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+}
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log("✅✅✅ Assertion Passed: ", arr1, "===", arr2);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: ", arr1, "!==", arr2);
+  }
+};
+//
+function middle(array) {
+  let a = [];
+  if (array.length <= 2) {
+    return a;
+  }
+  if (array.length % 2 !== 0) {
+    const index = Math.floor(array.length / 2);
+    const element = array[index];
+    a.push(element);
+    return a;
+  }
+  const index = array.length / 2;
+  const element1 = array[index];
+  const element2 = array[index - 1];
+  a.push(element2);
+  a.push(element1);
+  return a;
+}
+middle([1, 2, 3, 4, 5, 6]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
