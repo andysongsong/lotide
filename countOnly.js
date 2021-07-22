22e39fde6566905b74a6bc185c81157c685ada0c
andysongsong <sxj930916@gmail.com>
2021-07-22 01:46:55 +0000

    countletters

--- countOnly.js
+++ countOnly.js
@@ -43,6 +43,13 @@ const result1 = countOnly(firstNames, {
   Agouhanna: false,
 });
 
+console.log({
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
