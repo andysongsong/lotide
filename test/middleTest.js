58b33c4fddd3d8c3adb40a6b83eaf1a7b200f9a0
andysongsong <sxj930916@gmail.com>
2021-07-26 23:59:03 +0000

    chai mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,9 @@
+// const assertArraysEqual = require("../assertArraysEqual");
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+const assert = require("chai").assert;
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
 const tail = require("../tail");
-const eqArrays = require("../eqArrays");
-const assertEqual = require("../assertEqual");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+const assert = require("chai").assert;
+// const eqArrays = require("../eqArrays");
+// const assertEqual = require("../assertEqual");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-assertEqual(eqArrays(tail(words), ["Lighthouse", "Labs"]), true);
+// assertEqual(eqArrays(tail(words), ["Lighthouse", "Labs"]), true);
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+});
