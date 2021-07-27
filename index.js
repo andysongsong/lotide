a77f68664e053f574e32f8bb26f35b0b9977baba
andysongsong <sxj930916@gmail.com>
2021-07-27 00:11:52 +0000

    exports all

---
+++ index.js
@@ -0,0 +1,36 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const findKeyByValue = require("./findKeyByValue");
+const findKeys = require("./findKeys");
+const flatten = require("./flatten");
+const map = require("./map");
+const takeUntil = require("./middle");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const without = require("./middle");
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+};
