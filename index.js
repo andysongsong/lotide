a63fc9552afb5c00c3108f539988e5dbc7e387fb
andysongsong <sxj930916@gmail.com>
2021-07-27 00:16:07 +0000

    change

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const head = require("./head");
 const tail = require("./tail");
 const middle = require("./middle");
 const countLetters = require("./countLetters");
-const countOnly = require("./countOnly");
-const findKeyByValue = require("./findKeyByValue");
 const findKeys = require("./findKeys");
+const countOnly = require("./countOnly");
+const assertArraysEqual = require("./assertArraysEqual");
 const flatten = require("./flatten");
 const map = require("./map");
-const takeUntil = require("./middle");
-const assertArraysEqual = require("./assertArraysEqual");
-const assertEqual = require("./assertEqual");
-const assertObjectsEqual = require("./assertObjectsEqual");
+const findKeyByValue = require("./findKeyByValue");
 const eqArrays = require("./eqArrays");
-const eqObjects = require("./eqObjects");
+const takeUntil = require("./middle");
+const assertEqual = require("./assertEqual");
 const without = require("./middle");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const eqObjects = require("./eqObjects");
 
 module.exports = {
   head,
   tail,
   middle,
-  assertArraysEqual,
   assertEqual,
+  countOnly,
   eqArrays,
   assertObjectsEqual,
   countLetters,
-  countOnly,
   eqObjects,
   findKey,
-  findKeyByValue,
+  assertArraysEqual,
   flatten,
+  findKeyByValue,
+  takeUntil,
   letterPositions,
   map,
-  takeUntil,
   without,
 };
